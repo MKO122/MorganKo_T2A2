f47f659c82310062d811a54f592f228248ae0de3
MKO122 <morgan.and.ko@hotmail.com>
2022-03-17 12:07:56 +1100

    Navbar stylised

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def select_alert_subclass(input)
+        case input
+            #  The input is the colour from bootstrap
+        when 'notice'
+          return 'success'
+        when 'alert'
+          return 'warning'
+        when 'error'
+          return 'danger'
+        else
+          return 'info'
+        end 
+    end 
 end
