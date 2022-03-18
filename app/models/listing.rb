c7bb95de4bb43dd1899c0025daa6ae7793c20f35
MKO122 <morgan.and.ko@hotmail.com>
2022-03-18 21:22:28 +1100

    Set up listing models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  enum condition: {longing_stares_into_the_horizon: 1, sisyphus: 2, left_at_the_altar: 3, period: 4]
+  enum condition: {longing_stares_into_the_horizon: 1, sisyphus: 2, left_at_the_altar: 3, period: 4}
   #unstarted, part way, almost finished, completeduser
 end
