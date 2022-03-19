80fa51091011a02b220681b508c0b547d47c0a26
MKO122 <morgan.and.ko@hotmail.com>
2022-03-19 14:59:16 +1100

    Added form and updating/destroy functionality to listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,14 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :set_form_vars, only: [:new, :edit]
+
   def index
     @listings = Listing.all
   end
 
   def show
-    @listing = Listing.find(params[:id])
+    # @listing = Listing.find(params[:id])
   end
 
   def new
@@ -14,7 +16,13 @@ class ListingsController < ApplicationController
   end
 
   def create
-
+    @listing = current_user.listings.new(listing_params)
+    if @listing.save 
+      redirect_to @listing, notice: "Listing successfully created"
+    else
+      set_form_vars
+      render "new", notice: "Something went wrong"
+    end 
   end
 
   def edit
@@ -22,13 +30,35 @@ class ListingsController < ApplicationController
   end
 
   def update
-
+    @listing.update(listing_params)
+    if @listing.save 
+      redirect_to @listing, notice: "Listing successfully updated"
+    else
+      set_form_vars
+      render "edit", notice: "Something went wrong"
+    end
   end
 
   def destroy
-
+    @listing.destroy
+    redirect_to listings_path, notice: "Succesfully deleted"
   end
 
   private
+
+  def listing_params
+    params.require(:listing).permit(:title, :price, :category_id, :condition, :description) 
+      #,:picture, feature_ids: [])
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def set_form_vars
+    @categories = Category.all
+    @conditions = Listing.conditions.keys
+    # @features = Feature.all
+  end
 
 end
